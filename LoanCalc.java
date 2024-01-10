231c31d61aa016118680740b8a8d9b797bfdb907
shirshani1 <shir.shani@post.runi.ac.il>
2024-01-10 13:28:04 +0200

    ,

--- LoanCalc.java
+++ LoanCalc.java
@@ -17,7 +17,9 @@ public class LoanCalc {
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-		System.out.print("Periodical payment, using brute force: " + bruteForceSolver(loan, rate, n, epsilon));
+		
+		// Computes the periodical payment using brute force search
+		System.out.print("Periodical payment, using brute force: ");
 		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println();
 		System.out.println("number of iterations: " + iterationCounter);
@@ -37,15 +39,18 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-		double g = loan/n;
+    	// Replace the following statement with your code
 		iterationCounter = 0;
-		while (endBalance(loan, rate, n, g) > 0) {
-			g+= epsilon;
-			iterationCounter++;
-		}  
-		int g1 = (int) (g * 100);
-		g = (double) g1 / 100;
-		return g;
+    	double g = loan / n;
+    	// bruteForcePayment += epsilon;
+
+    	while(endBalance(loan, rate, n, g) > 0)
+    	{
+    		g += epsilon;
+    		iterationCounter++;
+    	}
+
+    	return g;
     }
     
     /**
@@ -56,34 +61,49 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	double H = loan;
-    	double L = loan/n;
-    	double g = (L + H) / 2;
-    	iterationCounter = 0;
-    	while (H - L > epsilon) {
-			if (endBalance(loan, rate, n, g)* endBalance(loan, rate, n, L) > epsilon){
-			L = g;
-			}else{
-			H = g;
-			}
-			
-			g = (L + H) / 2;
-			iterationCounter++;
+    	// Replace the following statement with your code
+    	// Sets L and H to initial values such that 𝑓(𝐿) > 0, 𝑓(𝐻) < 0,
+		iterationCounter = 0;
+		double L = 0;
+        double H = loan;
 
-			}
-    	return g;
+        // Set initial guess g to the middle of L and H
+        double g = (L + H) / 2;
+
+        while ((H - L) > epsilon) {
+            iterationCounter++;
+
+            // Compute the values of the function at g, L and H
+            double fG = endBalance(loan, rate, n, g);
+            double fLo = endBalance(loan, rate, n, L);
+            double fHi = endBalance(loan, rate, n, H);
+
+            // Check the sign of the product of fG and fLo (or fG and fHi)
+            if (fG * fLo > 0) {
+                // Solution is between g and H
+                L = g;
+            } else {
+                // Solution is between L and g
+                H = g;
+            }
+
+            // Update g for the next iteration
+            g = (L + H) / 2;
+        }
+
+        // Return the approximate solution
+        return g;
     }
-	
+
 	/**
 	* Computes the ending balance of a loan, given the sum of the loan, the periodical
 	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
-		for (int i=1;i<= n ;i++ ) {
-		loan -= payment;
-		loan = loan*(1+(rate/100));	
-		}
-		
-    	return loan;
-	}
+        for (int i = 0; i < n; i++)
+		{
+			loan = (loan - payment) * (1 + (rate/100));
+        }
+        return loan;
+    }
 }
